d2d65b13b8760759ce91b5c9583dca8eaced2d60
Tea Cup <35646791+TheTeaCup@users.noreply.github.com>
2018-10-29 20:52:00 -0400

    Create discord.js

---
+++ discord.js
@@ -0,0 +1,56 @@
+const db = require('quick.db'),
+      Discord = require('discord.js');
+const fs = require('fs');
+let prefix = "+"
+module.exports = function(client) {  
+  
+    client.on('ready', () => {
+    console.log('Discord Instance Launched');
+    console.log(`--- Logged in as ${client.user.tag} ---`)
+  });
+  
+      fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    let eventFunction = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+
+    client.on(eventName, (...args) => eventFunction.run(client, ...args));
+  });
+});
+  
+    client.on("message", async message => {
+        if(message.author.bot) return;
+        if(message.channel.type === 'dm') return ;
+   
+ 
+
+
+
+ 
+    
+     if (message.content == `<@${client.user.id}>`) {
+        const embed2 = new Discord.RichEmbed()
+         .setTitle("You Ring?")
+          .setColor("BLURPLE")
+          .setDescription(`Need Help? then do ${prefix}help!`)
+    
+    message.channel.send(embed2)
+  }
+  
+                                           
+  if (message.content.indexOf(prefix) !== 0) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();  
+
+try{
+let commandFile = require(`./commands/${command}.js`);
+ commandFile.run(client, message, args);
+if(!commandFile) return;
+} catch (err) {
+}
+       
+});
+  
+}
