7a2cb782bc3739f774bae4cda496932239fd6b39
Tea Cup <35646791+TheTeaCup@users.noreply.github.com>
2018-11-02 20:53:58 -0400

    Update hunt.js
    added cool down

--- commands/hunt.js
+++ commands/hunt.js
@@ -1,8 +1,32 @@
 const db = require('quick.db');
 const Discord = require("discord.js");
 const { get } = require("snekfetch");
+const ms = require('parse-ms');
 //let devs = ["338192747754160138","364007557045551106","297096161842429963"]
 module.exports.run = async (client, message, args) => {
+  
+     let cooldown = 900000;
+    
+
+    let lastDaily = await db.fetch(`lastDailys_${message.author.id}`);
+  
+    try {
+     
+
+
+  if (lastDaily !== null && cooldown - (Date.now() - lastDaily) > 0) {
+      
+        let timeObj = ms(cooldown - (Date.now() - lastDaily))
+
+const lembed = new Discord.RichEmbed()
+.setAuthor("PS ▸ 🚀", client.user.avatarURL)
+ .setColor(0x36393e)
+ .setFooter(`Uh-oh.. not again! | 🚀 🢒 PS`, message.author.avatarURL)
+ .setDescription("**!** |  ⬪ Whoopsies! I have searched far and wide, but I have received an error..")
+ 
+ .addField("🢒 Error Detected | ❗", "HEY! You will receive your daily income in | " + `**${timeObj.hours} hours, ${timeObj.minutes} minutes**`);
+              return message.channel.send(lembed)
+    } else {
   
       const slot = ["You shot a Deer!","You shot a Bear","You caught, uh.. whats this?","You went deep into the mountains, just to fall down a hill..","Can you hold a Gun?\n(YOU just shot a trash can)","\"What are you doing?\", you shot at a teeny,tiney bird"]
     const slotOne = slot[Math.floor(Math.random() * slot.length)];
@@ -19,5 +43,9 @@ amount = parseInt(amount)
   .setColor("BROWN")
   .setDescription(`${slotOne}\nAnd got paid \`${amount}\``)
    db.add(`userBalance_${message.author.id}`, amount * 1)
+       db.set(`lastDailys_${message.author.id}`, Date.now());
   message.channel.send(embed)
+}
+ } catch(err) {
+    }
 }
